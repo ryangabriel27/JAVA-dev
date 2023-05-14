a35e3100551d1347243c6bc64713c56aa35b5b9c
ryangabriel27 <123770312+ryangabriel27@users.noreply.github.com>
2023-05-14 14:33:52 +0000

    aaaaa

--- Matriz/ListaExtra.java
+++ Matriz/ListaExtra.java
@@ -1,9 +1,11 @@
 package Matriz;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class ListaExtra {
     Random rd = new Random();
+    Scanner sc = new Scanner(System.in);
 
     public void exercicio1() {
         int matriz[][] = new int[4][4];
@@ -25,14 +27,14 @@ public class ListaExtra {
         int matriz[][] = new int[4][4];
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[0].length; j++) {
-                matriz[i][j] = rd.nextInt(20);
+                matriz[i][j] = rd.nextInt(50);
             }
         }
 
         for (int i = 0; i < matriz.length; i++) {
             System.out.print(" | ");
             for (int j = 0; j < matriz[0].length; j++) {
-                System.out.print(" "+matriz[i][j]+" ");
+                System.out.print(" " + matriz[i][j] + " ");
             }
             System.out.println(" | ");
         }
@@ -49,10 +51,66 @@ public class ListaExtra {
                 }
             }
         }
-        System.out.println("O maior termo da matriz é: "+maior+" e está na linha : "+linha+" e coluna: "+coluna);
+        System.out.println(
+                "O maior termo da matriz é: " + maior + " e está na linha : " + linha + " e coluna: " + coluna);
     }
+
     public void exercicio3() {
         int matriz[][] = new int[5][5];
-        
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = rd.nextInt(20);
+            }
+        }
+        System.out.print("Digite um valor para X :");
+        int valorX = sc.nextInt();
+        System.out.println("=========================");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print(" | ");
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(" " + matriz[i][j] + " ");
+            }
+            System.out.println(" | ");
+        }
+        System.out.println("=========================");
+        int linha = 0;
+        int coluna = 0;
+        int x = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (matriz[i][j] == valorX) {
+                    linha = i;
+                    coluna = j;
+                    x++;
+                }
+            }
+        }
+        if (x != 0) {
+            System.out.println("O valor X na matriz está na linha: " + linha + " e na coluna: " + coluna);
+        } else {
+            System.out.println(" A matriz não tem o valor X");
+        }
     }
-}
+    public void exercicio4() {
+        int matriz[][] = new int[10][10];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (i == j) {
+                    matriz[i][j] = (3*i*i) - 1;
+                } else if ( i > j) {
+                    matriz[i][j] = (4*i*i*i) - (5*j*j);
+                } else {
+                    matriz[i][j] = (2*i) + (7*j) - 2;
+                }
+            }
+        }
+        System.out.println("=========================");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print(" | ");
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(" " + matriz[i][j] + " ");
+            }
+            System.out.println(" | ");
+        }
+        System.out.println("=========================");
+    }
